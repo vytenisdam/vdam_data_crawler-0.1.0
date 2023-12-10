04bc76ab99f0efda5b35fa91640737a40cbd1422
Vytenis Dambrauskas <vytenisdambrauskas@gmail.com>
2023-12-10 10:55:46 +0200

    Commit 1

--- vdam_data_crawler 0.1.0/main.py
+++ vdam_data_crawler 0.1.0/main.py
@@ -1,16 +1,46 @@
-# This is a sample Python script.
+from concurrent.futures import ThreadPoolExecutor, wait
+from queue import Queue
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from lxml.etree import HTML
+from requests import get
+
+visited_pages = set()
+visited_articles = set()
+
+pages_queue = Queue()
+articles_queue = Queue()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def query_to_url(query: str) -> str:
+    return f"https://www.lrytas.lt/search?q={query}"
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def process_page(page_url: str):
+    try:
+        response = get(page_url)
+        tree = HTML(response.text)
+        for page_link in tree.xpath(
+                "//a[contains(@class, 'LPagination__button')]/@href"
+        ):
+            page_url = "https://www.lrytas.lt" + page_link
+            if page_url not in visited_pages:
+                pages_queue.put(page_url)
+                visited_pages.add(page_url)
+        for article_link in tree.xpath("//a[@class='LPostContent__anchor']/@href"):
+            article_url = "https://www.lrytas.lt" + article_link
+            if article_url not in visited_articles:
+                visited_articles.add(article_url)
+                articles_queue.put(article_url)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    except Exception:
+        print("Error retrieving url")
+
+
+if __name__ == "__main__":
+    process_page(query_to_url("rytas"))
+    with ThreadPoolExecutor(10) as pool:
+        futures = [pool.submit(process_page, query_to_url("rytas"))]
+        while wait(futures):
+            while not pages_queue.empty():
+                page = pages_queue.get()
+                futures.append(pool.submit(process_page, page))
