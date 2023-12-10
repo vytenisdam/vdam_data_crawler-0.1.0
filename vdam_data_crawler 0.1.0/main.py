b99283839d8305abf93c773aca30adbce7c21bc4
Vytenis Dambrauskas <vytenisdambrauskas@gmail.com>
2023-12-10 11:11:20 +0200

    Added async crawling with threads to main.py

--- vdam_data_crawler 0.1.0/main.py
+++ vdam_data_crawler 0.1.0/main.py
@@ -1,4 +1,4 @@
-from concurrent.futures import ThreadPoolExecutor, wait
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from queue import Queue
 
 from lxml.etree import HTML
@@ -15,32 +15,39 @@ def query_to_url(query: str) -> str:
     return f"https://www.lrytas.lt/search?q={query}"
 
 
-def process_page(page_url: str):
+def process_page(page_url: str) -> str:
+    print(page_url)
     try:
         response = get(page_url)
         tree = HTML(response.text)
         for page_link in tree.xpath(
                 "//a[contains(@class, 'LPagination__button')]/@href"
         ):
-            page_url = "https://www.lrytas.lt" + page_link
-            if page_url not in visited_pages:
-                pages_queue.put(page_url)
-                visited_pages.add(page_url)
+            full_page_url = "https://www.lrytas.lt" + page_link
+            if full_page_url not in visited_pages:
+                pages_queue.put(full_page_url)
+                visited_pages.add(full_page_url)
         for article_link in tree.xpath("//a[@class='LPostContent__anchor']/@href"):
             article_url = "https://www.lrytas.lt" + article_link
             if article_url not in visited_articles:
                 visited_articles.add(article_url)
                 articles_queue.put(article_url)
 
-    except Exception:
-        print("Error retrieving url")
+    except Exception as e:
+        print("Error retrieving url", e)
 
 
 if __name__ == "__main__":
-    process_page(query_to_url("rytas"))
-    with ThreadPoolExecutor(10) as pool:
-        futures = [pool.submit(process_page, query_to_url("rytas"))]
-        while wait(futures):
-            while not pages_queue.empty():
-                page = pages_queue.get()
-                futures.append(pool.submit(process_page, page))
+    initial_url = query_to_url("rytas")
+    pages_queue.put(initial_url)
+    visited_pages.add(initial_url)
+    with ThreadPoolExecutor(10) as executor:
+        futures = set()
+        while not pages_queue.empty() and len(futures) < 10:
+            page = pages_queue.get()
+            future = executor.submit(process_page, page)
+            futures.add(future)
+        done = as_completed(futures, timeout=None)
+        for future in done:
+            futures.remove(future)
+    print("Finished processing all pages.")
