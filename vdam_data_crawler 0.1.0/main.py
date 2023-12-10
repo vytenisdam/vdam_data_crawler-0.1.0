f1709609255eac74c21048e2ba9892e6234c076b
Vytenis Dambrauskas <vytenisdambrauskas@gmail.com>
2023-12-10 13:01:42 +0200

    Working page walking prototype

--- vdam_data_crawler 0.1.0/main.py
+++ vdam_data_crawler 0.1.0/main.py
@@ -1,27 +1,31 @@
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from queue import Queue
-
 from lxml.etree import HTML
 from requests import get
+from threading import Lock
 
+# Global variables
 visited_pages = set()
 visited_articles = set()
-
 pages_queue = Queue()
 articles_queue = Queue()
+
+lock = Lock()
 
 
 def query_to_url(query: str) -> str:
     return f"https://www.lrytas.lt/search?q={query}"
 
 
-def process_page(page_url: str) -> str:
-    print(page_url)
+def process_page(page_url: str):
+    with lock:
+        print(page_url)
+    # global visited_pages, visited_articles, pages_queue, articles_queue
     try:
         response = get(page_url)
         tree = HTML(response.text)
         for page_link in tree.xpath(
-                "//a[contains(@class, 'LPagination__button')]/@href"
+            "//a[contains(@class, 'LPagination__button')]/@href"
         ):
             full_page_url = "https://www.lrytas.lt" + page_link
             if full_page_url not in visited_pages:
@@ -32,22 +36,28 @@ def process_page(page_url: str) -> str:
             if article_url not in visited_articles:
                 visited_articles.add(article_url)
                 articles_queue.put(article_url)
-
     except Exception as e:
-        print("Error retrieving url", e)
+        print("Error retrieving url:", e)
 
 
-if __name__ == "__main__":
+def main():
     initial_url = query_to_url("rytas")
     pages_queue.put(initial_url)
     visited_pages.add(initial_url)
-    with ThreadPoolExecutor(10) as executor:
+    with ThreadPoolExecutor(max_workers=10) as executor:
         futures = set()
-        while not pages_queue.empty() and len(futures) < 10:
-            page = pages_queue.get()
-            future = executor.submit(process_page, page)
-            futures.add(future)
-        done = as_completed(futures, timeout=None)
-        for future in done:
-            futures.remove(future)
+        while not pages_queue.empty() or futures:
+            # Submit new tasks from the queue
+            while not pages_queue.empty() and len(futures) < 10:
+                page = pages_queue.get()
+                future = executor.submit(process_page, page)
+                futures.add(future)
+            # Wait for the next future to complete
+            done = as_completed(futures, timeout=None)
+            for future in done:
+                futures.remove(future)
     print("Finished processing all pages.")
+
+
+if __name__ == "__main__":
+    main()
